cc090bb4b7489ef4659001e288b2194bfcdb4edb
saeidha <saeed221373@yahoo.com>
2025-09-09 17:34:38 +0330

    add jsons

--- bash.sh
+++ bash.sh
@@ -1,1 +1,13 @@
 forge inspect MonadPay bytecode > out/MonadPay.bytecode
+
+
+forge inspect src/Payable.sol:MonadPay abi --json > abi/MonadPay.json
+forge inspect src/Payable.sol:DepMonad abi --json > abi/DepMonad.json
+forge inspect src/Payable.sol:PaymeMonad abi --json > abi/PaymeMonad.json
+forge inspect src/Payable.sol:MonadAddBalance abi --json > abi/MonadAddBalance.json
+forge inspect src/Payable.sol:MonadNote abi --json > abi/MonadNote.json
+forge inspect src/Payable.sol:MonadSave abi --json > abi/MonadSave.json
+forge inspect src/Payable.sol:PayEventMonad abi --json > abi/PayEventMonad.json
+forge inspect src/Payable.sol:MonadPayTrack abi --json > abi/MonadPayTrack.json
+forge inspect src/Payable.sol:PayCountMonad abi --json > abi/PayCountMonad.json
+forge inspect src/Payable.sol:MonadContribute abi --json > abi/MonadContribute.json
